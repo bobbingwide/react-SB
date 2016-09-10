0046ca61d0afb2e7b12c4b436d84b56360e159f2
Herb Miller <herb@bobbingwide.com>
2016-09-10 11:04:54 +0100

    Create an App component above Board and start developing the REST code with Bigram

--- app/App.js
+++ app/App.js
@@ -1,8 +1,47 @@
+/**
+ * (C) Copyright Bobbing Wide 2016
+ * 
+ * We assume that the 'api' appends the wp-json part to the url 
+ * 
+ Want to get your site's posts? Simply send a GET request to 
+ http://qw/bigram/wp-json/wp/v2/posts. Update user with ID 4? Send a PUT request to /wp-json/wp/v2/users/4. Get all posts with the search term "awesome"? GET /wp-json/wp/v2/posts?filter[s]=awesome. It's that easy.
+ */
+ 
 import React, { Component } from 'react';
 import {render} from 'react-dom';
 import Board from './Board';
+import Bigrams from './Bigrams';
 
-render( <Board />, document.getElementById( 'root' ) ) ;
+import api from 'wordpress-rest-api-oauth-1';
+
+class App extends Component {
+
+  constructor() {
+		super()
+		this.state = {
+			posts: [],
+			isLoadingPosts: false,
+			url: 'http://qw/bigram',
+			site: null,
+			user: null,
+			category: null,
+		}
+	}
+
+  render() {
+		let bigram = new Bigrams;
+		bigram.onConnect();
+	 
+		return( 
+			<div className="app">
+			<Board />
+			</div> 
+			);
+
+	}
+}
+
+render( <App />, document.getElementById( 'root' ) );
 
 
 
---
+++ app/Bigrams.js
@@ -0,0 +1,48 @@
+/**
+ * (C) Copyright Bobbing Wide 2016
+ *
+ * Bigrams - these are the posts that we load from the server
+ *
+ */
+ 
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+ 
+import api from 'wordpress-rest-api-oauth-1';
+
+const BIGRAM_URL = "http://qw/bigram/"
+
+const demoApi = new api({
+    url: BIGRAM_URL
+			});
+
+class Bigrams extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			posts: [],
+			isLoadingPosts: false,
+			url: BIGRAM_URL,
+		};
+	}
+
+	componentWillMount() {
+			this.onConnect( this.state.url );
+	}
+
+	componentWillUnmount() {
+	}
+
+	onConnect( url ) {
+		demoApi.get( '/wp/v2/bigram' ).then( posts => { console.log( posts ) });
+	}
+
+
+
+
+}
+
+
+export default Bigrams;
+
